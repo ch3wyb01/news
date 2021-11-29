6ce14e58afb2e688ed7ca16a74ebd49c88a84cb6
Becca <beccasullivan30@gmail.com>
2021-11-29 15:31:11 +0000

    set up articles router

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,6 +1,10 @@
+const articlesRouter = require('./articles.router');
 const topicsRouter = require('./topics.router');
+
 const apiRouter = require('express').Router();
 
 apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/articles', articlesRouter);
+
 
 module.exports = apiRouter;
