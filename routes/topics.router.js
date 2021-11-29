faf8c4bc5f49e44931775966a8b9000b565d2eaa
Becca <beccasullivan30@gmail.com>
2021-11-29 15:09:51 +0000

    get route

--- routes/topics.router.js
+++ routes/topics.router.js
@@ -1,5 +1,7 @@
+const { getTopics } = require('../controllers/topics.controllers');
+
 const topicsRouter = require('express').Router();
 
-
+topicsRouter.route('/').get(getTopics);
 
 module.exports = topicsRouter;
