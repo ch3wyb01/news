3d94d02f06c1e0c39cc81cf1c67d794d7bfd989f
Becca <beccasullivan30@gmail.com>
2021-11-29 17:45:19 +0000

    use express.json

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require("./errors/errors");
+const {
+  handleServerErrors,
+  handlePsqlErrors,
+  handleCustomErrors,
+} = require("./errors/errors");
 const apiRouter = require("./routes/api.router");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
 app.use(handlePsqlErrors);
-app.use(handleCustomErrors)
+app.use(handleCustomErrors);
 app.use(handleServerErrors);
 module.exports = app;
