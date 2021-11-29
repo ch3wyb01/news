e81ff349b503bae37dcab7bf17889830fd68ed16
Becca <beccasullivan30@gmail.com>
2021-11-29 19:11:10 +0000

    cast COUNT as integer

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,14 +2,14 @@ const db = require("../db/connection");
 
 exports.selectArticleById = async (article_id) => {
   const { rows } = await db.query(
-    `SELECT articles.*, COUNT(comments.article_id) AS comment_count
+    `SELECT articles.*, CAST(COUNT(comments.article_id) AS int) AS comment_count
     FROM articles
     JOIN comments ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
     GROUP BY articles.article_id;`,
     [article_id]
   );
-  
+
   const article = rows[0];
 
   if (!article) {
