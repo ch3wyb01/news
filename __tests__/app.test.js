25d8b1c08440a9253931809412082e679b468a9e
Becca <beccasullivan30@gmail.com>
2021-11-29 14:32:42 +0000

    set up app and supertest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,8 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const { seed } = require('../db/seeds/seed.js');
+const request = require('supertest');
+const app = require('../app');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
