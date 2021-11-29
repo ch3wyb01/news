29439ccab1782376c384d61e9a76ec463c54b864
Becca <beccasullivan30@gmail.com>
2021-11-29 14:53:57 +0000

    test for 200 GET topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,27 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
-const request = require('supertest');
-const app = require('../app');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  test("200: returns array of topic objects with slug and description keys", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.topics).toHaveLength(3);
+        body.topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
