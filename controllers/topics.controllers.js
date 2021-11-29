ba616e611dcc6463b2a846d2e9acda659fbe5e39
Becca <beccasullivan30@gmail.com>
2021-11-29 15:11:29 +0000

    topics controller

---
+++ controllers/topics.controllers.js
@@ -0,0 +1,7 @@
+const { selectTopics } = require("../models/topics.models");
+
+exports.getTopics = async (req, res, next) => {
+    const topics = await selectTopics();
+    
+    res.status(200).send({ topics });
+};
