45c4f92c0b15fd33213a9b757f8b997884d86698
Becca <beccasullivan30@gmail.com>
2021-11-29 15:22:58 +0000

    handle 500 errors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const apiRouter = require('./routes/api.router');
+const express = require("express");
+const { handleServerErrors } = require("./errors/errors");
+const apiRouter = require("./routes/api.router");
 
 const app = express();
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
+app.use(handleServerErrors);
 module.exports = app;
