90ee670a78f8176c12c89ae58fddbf34f301bdca
Becca <beccasullivan30@gmail.com>
2021-11-29 16:51:32 +0000

    successful GET article by Id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,3 +25,25 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: returns an article object with all relevant keys", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual(
+          expect.objectContaining({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            body: "I find this existence challenging",
+            votes: 100,
+            topic: 'mitch',
+            author: 'butter_bridge',
+            created_at: '2020-07-09T20:11:00.000Z',
+            comment_count: '11'
+          })
+        );
+      });
+  });
+});
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,3 +1,16 @@
+const db = require("../db/connection");
+
 exports.selectArticleById = async (article_id) => {
-    
+
+  const { rows } = await db.query(
+    `SELECT articles.*, COUNT(comments.article_id) AS comment_count
+    FROM articles
+    JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`,
+    [article_id]
+  );
+  
+  return rows[0];
 };
+
