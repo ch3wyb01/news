feb5bb189ed6b0c1230d03a3fd71d83f40be3a1c
Becca <beccasullivan30@gmail.com>
2021-11-30 12:37:07 +0000

    error handling for invalid path

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Path not found" });
+});
+
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
