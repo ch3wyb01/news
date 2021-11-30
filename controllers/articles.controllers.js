90cb1509271888be61fe6caae41fbaa896aa2b7b
Becca <beccasullivan30@gmail.com>
2021-11-30 12:11:02 +0000

    PATCH error handling

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,7 @@
-const { selectArticleById, updateArticleById } = require("../models/articles.models");
+const {
+  selectArticleById,
+  updateArticleById,
+} = require("../models/articles.models");
 
 exports.getArticleById = async (req, res, next) => {
   try {
@@ -11,10 +14,14 @@ exports.getArticleById = async (req, res, next) => {
 };
 
 exports.patchArticleById = async (req, res, next) => {
-  const {inc_votes} = req.body;
-  const {article_id} = req.params;
+  try {
+    const { inc_votes } = req.body;
+    const { article_id } = req.params;
 
-  const article = await updateArticleById(inc_votes, article_id);
-  
-  res.status(200).send({article});
-}
+    const article = await updateArticleById(inc_votes, article_id);
+
+    res.status(200).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
