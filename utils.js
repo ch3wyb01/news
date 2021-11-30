2b26667154cbd6b6b2d2cc265dc4bbd38d8c7566
Becca <beccasullivan30@gmail.com>
2021-11-30 18:58:38 +0000

    checkExists function

---
+++ utils.js
@@ -0,0 +1,13 @@
+const format = require("pg-format");
+const db = require("./db/connection");
+
+exports.checkExists = async (table, column, value) => {
+  const queryStr = format(`
+    SELECT * FROM %I
+    WHERE %I = $1;`, table, column);
+  const { rows } = await db.query(queryStr, [value]);
+
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: `Resource not found` });
+  }
+};
