1585d46d193245211b537685d2358223e964b5f4
Becca <beccasullivan30@gmail.com>
2021-11-30 12:17:59 +0000

    refactor to async

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,89 +8,78 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
-  test("200: returns array of topic objects with slug and description keys", () => {
-    return request(app)
+  test("200: returns array of topic objects with slug and description keys", async () => {
+    const { body } = await request(app)
       .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.topics).toHaveLength(3);
-        body.topics.forEach((topic) => {
-          expect(topic).toEqual(
-            expect.objectContaining({
-              slug: expect.any(String),
-              description: expect.any(String),
-            })
-          );
-        });
-      });
+      .expect(200);
+    expect(body.topics).toHaveLength(3);
+    body.topics.forEach((topic) => {
+      expect(topic).toEqual(
+        expect.objectContaining({
+          slug: expect.any(String),
+          description: expect.any(String),
+        })
+      );
+    });
   });
 });
 
 describe("GET /api/articles/:article_id", () => {
-  test("200: returns an article object with all relevant keys", () => {
-    return request(app)
+  test("200: returns an article object with all relevant keys", async () => {
+    const { body } = await request(app)
       .get("/api/articles/1")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.article).toEqual(
-          expect.objectContaining({
-            article_id: 1,
-            title: "Living in the shadow of a great man",
-            body: "I find this existence challenging",
-            votes: 100,
-            topic: "mitch",
-            author: "butter_bridge",
-            created_at: "2020-07-09T20:11:00.000Z",
-            comment_count: 11,
-          })
-        );
-      });
+      .expect(200);
+    expect(body.article).toEqual(
+      expect.objectContaining({
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        body: "I find this existence challenging",
+        votes: 100,
+        topic: "mitch",
+        author: "butter_bridge",
+        created_at: "2020-07-09T20:11:00.000Z",
+        comment_count: 11,
+      })
+    );
   });
-  test("400: returns bad request message when passed invalid article_id", () => {
-    return request(app)
+  test("400: returns bad request message when passed invalid article_id", async () => {
+    const { body } = await request(app)
       .get("/api/articles/beans")
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Invalid input");
-      });
+      .expect(400);
+    expect(body.msg).toBe("Invalid input");
   });
-  test("404: returns not found message when passed valid but non-existent article_id", () => {
-    return request(app)
+  test("404: returns not found message when passed valid but non-existent article_id", async () => {
+    const { body } = await request(app)
       .get("/api/articles/100")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Article not found");
-      });
+      .expect(404);
+    expect(body.msg).toBe("Article not found");
   });
 });
 
 describe("PATCH /api/articles/:article_id", () => {
-  test("200: returns article with updated votes", () => {
+  test("200: returns article with updated votes", async () => {
     const incrementer = { inc_votes: 2 };
-    return request(app)
+    const { body } = await request(app)
       .patch("/api/articles/1")
       .send(incrementer)
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.article).toEqual({
-          article_id: 1,
-          title: "Living in the shadow of a great man",
-          body: "I find this existence challenging",
-          votes: 102,
-          topic: "mitch",
-          author: "butter_bridge",
-          created_at: "2020-07-09T20:11:00.000Z",
-          comment_count: 11,
-        });
-      });
+      .expect(200);
+    expect(body.article).toEqual({
+      article_id: 1,
+      title: "Living in the shadow of a great man",
+      body: "I find this existence challenging",
+      votes: 102,
+      topic: "mitch",
+      author: "butter_bridge",
+      created_at: "2020-07-09T20:11:00.000Z",
+      comment_count: 11,
+    });
   });
-  test("400: returns error message if no inc_votes included in body", () => {
-    const body = {};
-    return request(app)
+  test("400: returns error message if no inc_votes included in body", async () => {
+    const incrementer = {};
+    const { body: { msg } } = await request(app)
       .patch("/api/articles/2")
-      .expect(400)
-      .then(({ body: { msg } }) => {
-        expect(msg).toBe("No votes change inputted");
-      });
+      .send(incrementer)
+      .expect(400);
+    expect(msg).toBe("No votes change inputted");
   });
 });
