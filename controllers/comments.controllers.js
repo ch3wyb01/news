cb09614f5a35e27d6072d23f5cb6724d2e1f86e8
Becca <beccasullivan30@gmail.com>
2021-12-01 11:54:43 +0000

    check article Id exists

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,7 +1,16 @@
 const { selectCommentsByArticleId } = require("../models/comments.models");
+const { checkExists } = require("../utils");
 
 exports.getCommentsByArticleId = async (req, res, next) => {
-    const {article_id} = req.params;
-    const comments = await selectCommentsByArticleId(article_id);
-    res.status(200).send({comments});
-}
+  try {
+    const { article_id } = req.params;
+    const values = await Promise.all([
+      selectCommentsByArticleId(article_id),
+      checkExists("articles", "article_id", article_id),
+    ]);
+    const comments = values[0];
+    res.status(200).send({ comments });
+  } catch (err) {
+    next(err);
+  }
+};
