564b4d645042884295c385d9fd46d337a15bd2ae
Becca <beccasullivan30@gmail.com>
2021-12-01 12:12:35 +0000

    error handling for invalid article ID

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,6 +1,9 @@
 const db = require("../db/connection");
 
 exports.selectCommentsByArticleId = async (article_id) => {
+  if (isNaN(article_id)) {
+    return Promise.reject({ status: 400, msg: "Invalid article ID" });
+  }
   const { rows } = await db.query(
     `SELECT * FROM comments
     WHERE article_id = $1;`,
