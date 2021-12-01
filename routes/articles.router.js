9da5b1d7bbd01eb8fe01e4d7978e637992e6c346
Becca <beccasullivan30@gmail.com>
2021-12-01 11:08:30 +0000

    set up get comments by article Id route

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -1,9 +1,12 @@
 const { getArticleById, patchArticleById, getArticles } = require("../controllers/articles.controllers");
+const { getCommentsByArticleId } = require("../controllers/comments.controllers");
 
 const articlesRouter = require("express").Router();
 
 articlesRouter.route('/:article_id').get(getArticleById).patch(patchArticleById);
 
 articlesRouter.route('/').get(getArticles);
+
+articlesRouter.route('/:article_id/comments').get(getCommentsByArticleId);
 
 module.exports = articlesRouter;
