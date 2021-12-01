15d4529bbb9656c0c77b6b5592f27b8a7e487335
Becca <beccasullivan30@gmail.com>
2021-12-01 12:42:54 +0000

    add post comment route

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -1,5 +1,5 @@
 const { getArticleById, patchArticleById, getArticles } = require("../controllers/articles.controllers");
-const { getCommentsByArticleId } = require("../controllers/comments.controllers");
+const { getCommentsByArticleId, postCommentByArticleId } = require("../controllers/comments.controllers");
 
 const articlesRouter = require("express").Router();
 
@@ -7,6 +7,6 @@ articlesRouter.route('/:article_id').get(getArticleById).patch(patchArticleById)
 
 articlesRouter.route('/').get(getArticles);
 
-articlesRouter.route('/:article_id/comments').get(getCommentsByArticleId);
+articlesRouter.route('/:article_id/comments').get(getCommentsByArticleId).post(postCommentByArticleId);
 
 module.exports = articlesRouter;
