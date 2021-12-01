7a9762443d94b9f4df883f2ac1f31e4554eb2f0c
Becca <beccasullivan30@gmail.com>
2021-12-01 11:22:00 +0000

    200 for select comments

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,3 +1,7 @@
+const { selectCommentsByArticleId } = require("../models/comments.models");
+
 exports.getCommentsByArticleId = async (req, res, next) => {
-    
+    const {article_id} = req.params;
+    const comments = await selectCommentsByArticleId(article_id);
+    res.status(200).send({comments});
 }
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,3 +1,11 @@
-exports.selectCommentsByArticleId = async () => {
-    
-}
+const db = require("../db/connection");
+
+exports.selectCommentsByArticleId = async (article_id) => {
+  const { rows } = await db.query(
+    `SELECT * FROM comments
+    WHERE article_id = $1;`,
+    [article_id]
+  );
+
+  return rows;
+};
