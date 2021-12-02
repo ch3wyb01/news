cbfbce529b7bd7d5b3f53942c845453a7cc2ad1b
Becca <beccasullivan30@gmail.com>
2021-12-02 17:07:44 +0000

    create users router

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,6 +1,7 @@
 const topicsRouter = require("./topics.router");
 const articlesRouter = require("./articles.router");
 const commentsRouter = require("./comments.router");
+const usersRouter = require('./users.router');
 const { getEndpoints } = require("../controllers/api.controller");
 
 const apiRouter = require("express").Router();
@@ -9,5 +10,6 @@ apiRouter.route("/").get(getEndpoints);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;
