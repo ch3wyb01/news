7e8635d3ee4d07f1780cda49e9d3d98bf2bb37c7
Becca <beccasullivan30@gmail.com>
2021-12-03 12:43:04 +0000

    patch route

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,7 +1,7 @@
-const { deleteCommentById } = require("../controllers/comments.controllers");
+const { deleteCommentById, patchCommentById } = require("../controllers/comments.controllers");
 
 const commentsRouter = require("express").Router();
 
-commentsRouter.route("/:comment_id").delete(deleteCommentById);
+commentsRouter.route("/:comment_id").delete(deleteCommentById).patch(patchCommentById);
 
 module.exports = commentsRouter;
