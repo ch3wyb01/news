ba189cf1d06dadf89ce40a36d7cd03247286cb27
Becca <beccasullivan30@gmail.com>
2022-02-14 12:20:27 +0000

    change GET comments to return by newest first

--- models/comments.models.js
+++ models/comments.models.js
@@ -3,7 +3,8 @@ const db = require("../db/connection");
 exports.selectCommentsByArticleId = async (article_id) => {
   const { rows } = await db.query(
     `SELECT * FROM comments
-    WHERE article_id = $1;`,
+    WHERE article_id = $1
+    ORDER BY created_at DESC;`,
     [article_id]
   );
 
