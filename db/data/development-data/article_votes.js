0b559ede3363ce2cd86cf9f25aa3606e4cede2ad
Becca <beccasullivan30@gmail.com>
2022-02-17 13:11:36 +0000

    increase development data

--- db/data/development-data/article_votes.js
+++ db/data/development-data/article_votes.js
@@ -1,10 +1,42 @@
 module.exports = [
   {
+    article_id: 1,
+    username: "tickle122"
+  },
+  {
+    article_id: 1,
+    username: "cooljmessy"
+  },
+  {
+    article_id: 1,
+    username: "weegeebump"
+  },
+  {
+    article_id: 16,
+    username: "cooljmessy"
+  },
+  {
+    article_id: 35,
+    username: "tickle122"
+  },
+  {
+    article_id: 16,
+    username: "grumpy19"
+  },
+  {
     article_id: 3,
     username: "jessjelly",
   },
   {
     article_id: 6,
     username: "jessjelly",
+  },
+  {
+    article_id: 6,
+    username: "weegeebump",
+  },
+  {
+    article_id: 8,
+    username: "grumpy19",
   },
 ];
