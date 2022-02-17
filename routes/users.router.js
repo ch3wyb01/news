2a7a845b6c4bbf6ecc50aba2a14870097d0c77ff
Becca <beccasullivan30@gmail.com>
2022-02-17 12:47:34 +0000

    change route for DELETE article vote

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,13 +3,17 @@ const {
   getUserByUsername,
   postArticleVote,
   getVotedArticles,
-  deleteVotedArticle,
+  deleteArticleVote,
 } = require("../controllers/users.controller");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getUsers);
 usersRouter.route("/:username").get(getUserByUsername);
-usersRouter.route("/:username/voted_articles").post(postArticleVote).get(getVotedArticles).delete(deleteVotedArticle);
+usersRouter
+  .route("/:username/voted_articles")
+  .post(postArticleVote)
+  .get(getVotedArticles)
+  .delete(deleteArticleVote);
 
 module.exports = usersRouter;
