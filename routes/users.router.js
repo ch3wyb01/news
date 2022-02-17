f99a5c5f69e78e88fc79f814409469f113b93143
Becca <beccasullivan30@gmail.com>
2022-02-17 11:00:43 +0000

    create route for GET voted articles

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,12 +2,13 @@ const {
   getUsers,
   getUserByUsername,
   postVotedArticle,
+  getVotedArticles,
 } = require("../controllers/users.controller");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getUsers);
 usersRouter.route("/:username").get(getUserByUsername);
-usersRouter.route("/:username/voted_articles").post(postVotedArticle);
+usersRouter.route("/:username/voted_articles").post(postVotedArticle).get(getVotedArticles);
 
 module.exports = usersRouter;
