3eb566ea62bfe4bdbcbed1c11b97bd82faa38b64
Becca <beccasullivan30@gmail.com>
2022-02-17 11:38:34 +0000

    change error message for checkExists

--- utils.js
+++ utils.js
@@ -2,12 +2,18 @@ const format = require("pg-format");
 const db = require("./db/connection");
 
 exports.checkExists = async (table, column, value) => {
-  const queryStr = format(`
+  const resource = table.slice(0, -1);
+
+  const queryStr = format(
+    `
     SELECT * FROM %I
-    WHERE %I = $1;`, table, column);
+    WHERE %I = $1;`,
+    table,
+    column
+  );
   const { rows } = await db.query(queryStr, [value]);
 
   if (rows.length === 0) {
-    return Promise.reject({ status: 404, msg: `Resource not found in ${table}` });
+    return Promise.reject({ status: 404, msg: `${resource} does not exist` });
   }
 };
