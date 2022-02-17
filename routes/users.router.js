9ca78b46b6c7e5a0a716f1196516898a18958884
Becca <beccasullivan30@gmail.com>
2022-02-17 10:04:29 +0000

    add route for POST voted_article

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,11 +1,13 @@
 const {
   getUsers,
   getUserByUsername,
+  postVotedArticle,
 } = require("../controllers/users.controller");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getUsers);
 usersRouter.route("/:username").get(getUserByUsername);
+usersRouter.route("/:username/voted_articles").post(postVotedArticle);
 
 module.exports = usersRouter;
