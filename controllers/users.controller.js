64833493058444360ef644bd1da893968565bd0c
Becca <beccasullivan30@gmail.com>
2022-02-17 10:04:46 +0000

    create postVotedArticle

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,8 @@
-const { selectUsers, selectUserByUsername } = require("../models/users.models");
+const {
+  selectUsers,
+  selectUserByUsername,
+  insertVotedArticle,
+} = require("../models/users.models");
 const { checkExists } = require("../utils");
 
 exports.getUsers = async (req, res, next) => {
@@ -24,6 +28,20 @@ exports.getUserByUsername = async (req, res, next) => {
     const user = await selectUserByUsername(username);
 
     res.status(200).send({ user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postVotedArticle = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+
+    const { article_id } = req.body;
+
+    const article = await insertVotedArticle(article_id, username);
+
+    res.status(201).send({ article });
   } catch (err) {
     next(err);
   }
